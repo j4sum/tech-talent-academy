cc5d47554c8bc04cf66544c1bfd1ff79a82e00b1
Sumeyya Javaid <jasum@protonmail.com>
2022-10-16 13:31:13 +0100

    Delete week7-practice directory

--- week7-practice/week7task3/JavaScript/week7task3.js
+++
@@ -1,4 +0,0 @@
-
-function analyseForm(){
-    window.alert("Hello " + document.getElementById('fname').value + " " + document.getElementById('lname').value + ". Your email " + document.getElementById('email').value + " has been added to the mailing list.");
-}
